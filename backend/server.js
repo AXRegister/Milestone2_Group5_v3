bf1572a60514fc959c646cd53356c1941ea24d19
AXRegister <npoonai2020@my.fit.edu>
2023-05-03 22:02:49 -0400

    Added backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+//app.use(express.json());
+//app.use(cors());
+
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
